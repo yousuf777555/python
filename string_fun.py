569e119d73c4ab31555dafd77b20c35b7b1adef2
1st user <myousuf@gmail.com>
2022-07-27 23:52:55 +0530

    string functions

---
+++ string_fun.py
@@ -0,0 +1,33 @@
+#string function
+
+a="basic python programming"
+
+#string.endswith()
+
+b = a.endswith("new")
+print(b)
+
+#string count
+
+c=a.count("p")
+print(c)
+
+#string capitalized
+
+print(a.capitalize())
+
+#string upper and lower
+print(a.upper())
+print(a.lower())
+
+#string find
+
+print(a.find("p"))
+
+
+#string replace(old ,new)
+
+a=a.replace("basic","advanced")
+
+print(a)
+
