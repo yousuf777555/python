516f1e597a4f1fe24794e68bddbb297cd5777ebe
1st user <myousuf@gmail.com>
2022-07-27 23:51:31 +0530

    string slicing

---
+++ string.py
@@ -0,0 +1,22 @@
+#strlen
+
+a="basic python program"
+b=len(a)
+print(b)
+
+#slicing
+
+print(a[0:6])
+print(a[6:13])
+print(a[13:20])
+
+#slicing by skipping 
+
+print(a[0:20:2])
+
+#slicing using negative indexes
+
+print(a[-20:-15])
+print(a[-15:-8])
+
+
