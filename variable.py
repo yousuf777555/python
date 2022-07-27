0cda4702ec90cbf494ed23761ad4e317c7d56224
1st user <myousuf@gmail.com>
2022-07-27 23:50:16 +0530

    variable,typecasting

---
+++ variable.py
@@ -0,0 +1,31 @@
+# variable
+
+a=12
+print(a)
+b="python basic"
+print(b)
+c=12.123
+d="program"
+print(c)
+print("\n")
+
+# type
+
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+
+#combining two string
+
+print(b+d)
+
+#typecasting
+
+g = input("enter the string")
+print(type(g))
+h=int(g)
+print(type(h))
+
