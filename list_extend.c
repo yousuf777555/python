1c48581dbd81d2fc6ff02507763c8e9381093caf
1st user <myousuf@gmail.com>
2022-08-02 00:21:08 +0530

    extend function

---
+++ list_extend.c
@@ -0,0 +1,8 @@
+day =["monday","tuesday","wednesday"]
+day2 =["thursday","friday","saturday"]
+day.extend(day2)
+print(day)
+o/p
+
+['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
